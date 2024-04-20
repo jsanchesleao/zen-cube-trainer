7dedc107ef5108ea35e1b9994c43b51d835ecfaa
jsanchesleao <jsanchesleao@gmail.com>
2024-04-20 20:49:39 +0200

    fix build

--- src/components/trainer/index.tsx
+++ src/components/trainer/index.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react"
-import { CFOPCase, CFOPStep } from "../../model/CFOP"
+import { CFOPCase } from "../../model/CFOP"
 import { TrainerNotStarted } from "./trainerNotStarted"
 import { TrainerShowingQuestion } from "./trainerShowingQuestion"
 import { TrainerQuestion } from './trainer.types'
